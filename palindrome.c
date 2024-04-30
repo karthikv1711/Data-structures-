def3f5c77e3ae17184924fb66feae456125ab89c
karthikv1711 <164615028+karthikv1711@users.noreply.github.com>
2024-05-01 03:06:17 +0530

    palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -2,53 +2,61 @@
 #include<stdlib.h>
 #include<string.h>
 #define MAX 20
+
 char stack[MAX];
 int top=-1;
 
 void push(char item)
 {
-	stack[++top]=item;
-
+    if(top == MAX - 1) {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    stack[++top]=item;
 }
 
 char pop()
 {
-	return stack[--top];
+    if(top == -1) {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    return stack[top--];
 }
-
 
 int ispalindrome(char str[])
 {
-	int i,n;
-	n=strlen(str);
-	for(i=0;i<n/2;i++)
-	{
-		push(str[i]);
-	}
-	
-	if(i%2 == 1)
-		i=(n/2)+1;
-	else
-		i=n/2;
-		
-	while(str[i] != '\0')
-	{
-		char item;
-		item=pop();
-		if(item != str[i])
-			return 0;
-		 	 i++;
-	} return 1;
+    int i,n;
+    n=strlen(str);
+    for(i=0;i<n/2;i++)
+    {
+        push(str[i]);
+    }
+    
+    if(n%2 == 1)
+        i=(n/2)+1;
+    else
+        i=n/2;
+        
+    while(str[i] != '\0')
+    {
+        char item;
+        item=pop();
+        if(item != str[i])
+            return 0;
+             i++;
+    } 
+    return 1;
 }
 
 int main()
 {
-	char str[MAX];
-	printf("input string :\n");
-	scanf("%c",str);
-	if(ispalindrome(str))
-		printf("Given string is a palindrome");
-	else
-		printf("Given string is not a palindrome");
+    char str[MAX];
+    printf("input string :\n");
+    scanf("%s",str);
+    if(ispalindrome(str))
+        printf("Given string is a palindrome");
+    else
+        printf("Given string is not a palindrome");
+    return 0;
 }
-
